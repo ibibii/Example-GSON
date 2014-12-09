fed452880ffa9ac17ed7ea2705202ee556d2857e
cike2056 <whzhn1990@163.com>
2014-12-09 15:16:38 +0800

    GSON测试

---
+++ gen/com/qf/example_gson/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.qf.example_gson;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/qf/example_gson/entity/Others.java
@@ -0,0 +1,40 @@
+package com.qf.example_gson.entity;
+
+public class Others {
+	private String color ;
+	private String image ;
+	private String descr ;
+	private String id    ;
+	private String name  ;
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getDescr() {
+		return descr;
+	}
+	public void setDescr(String descr) {
+		this.descr = descr;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
